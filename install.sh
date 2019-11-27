92d3410d2a4c75c22095afb09cb2d0c3451ddad9
Chavoosh Ghasemi <chghasemi@cs.arizona.edu>
2019-11-27 15:43:22 -0800

    add libndn-cxx

--- install.sh
+++ install.sh
@@ -20,13 +20,31 @@ sudo apt-get update && wait
 sudo apt-get --fix-broken install && wait
 sudo apt-get install google-chrome-stable && wait
 
-# Install ndnping
+# Install ndnping and library
 if hash ndnping 2>/dev/null; then
   echo "ndnping already exist!"
 else
   sudo cp ndnping /usr/bin/
+fi
 
-# Clone the repository in home directory and run a quick test
-sudo apt-get install git && wait
-(cd ~ && git clone https://github.com/chavoosh/ivisa-headless-test && wait)
+# Clone the repository in home directory
+if [ ! -d "~/ivisa-headless-test" ]
+then
+  sudo apt-get install git && wait
+  (cd ~ && git clone https://github.com/chavoosh/ivisa-headless-test && wait)
+fi
+
+# Install ndnping and library
+if hash ndnping 2>/dev/null; then
+  echo "ndnping already exist!"
+else
+  (cd ~/ivisa-headless-test && sudo cp ./ndnping /usr/bin/ && wait)
+fi
+
+if [ ! -f "/usr/local/lib/libndn-cxx.so.0.6.6" ]
+then
+  (cd ~/ivisa-headless-test && sudo cp ./libndn-cxx.so.0.6.6 /usr/loacl/lib/ && wait)
+fi
+
+# Run a quick test
 (cd ~/ivisa-headless-test && nodejs headless-video-player.js https://ivisa.named-data.net/html/ndn_vs_ip.html && wait)
