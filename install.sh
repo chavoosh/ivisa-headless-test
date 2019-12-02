1555f633b7392a3e3082304624d706c7d4b1701b
Chavoosh Ghasemi <chghasemi@cs.arizona.edu>
2019-12-02 15:23:25 -0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # Install nodejs and npm
-sudo apt-get update && wait
-sudo apt-get install nodejs && wait
-sudo apt-get install npm && wait
+sudo apt-get update
+sudo apt-get install nodejs
+sudo apt-get install npm
 
 # Install packages
-npm i puppeteer-core && wait
+npm i puppeteer-core
 
 # Install Python
 sudo apt-get install python2.7
 
 # Install Google Chrome
 sudo bash -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
-wget https://dl.google.com/linux/linux_signing_key.pub && wait
-sudo apt-key add linux_signing_key.pub && wait
-rm linux_signing_key.pub && wait
-sudo apt-get update && wait
-sudo apt-get --fix-broken install && wait
-sudo apt-get install google-chrome-stable && wait
+wget https://dl.google.com/linux/linux_signing_key.pub
+sudo apt-key add linux_signing_key.pub
+rm linux_signing_key.pub
+sudo apt-get update
+sudo apt-get --fix-broken install
+sudo apt-get install google-chrome-stable
 
 # Clone the repository in home directory
-sudo apt-get install git && wait
+sudo apt-get install git
 (cd ~ && git clone https://github.com/chavoosh/ivisa-headless-test && wait)
 
 # Check ndnping
@@ -33,4 +33,4 @@ else
 fi
 
 # Run a quick test
-(cd ~/ivisa-headless-test && nodejs headless-video-player.js http://ivisa-icdn-1.dynu.net/html/ip-research-questions.html &)
+(cd ~/ivisa-headless-test && nodejs headless-video-player.js http://ivisa-icdn-1.dynu.net/html/ip-research-questions.html)
